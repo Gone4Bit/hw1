6e32fa97b04c8c015551ee74f61fe96e5aa5071f
Gone4Bit <42947248+Gone4Bit@users.noreply.github.com>
2018-09-25 14:54:28 -0400

    Add files via upload

---
+++ homework/sql dump/main_temp.sql
@@ -0,0 +1,15 @@
+insert into main.temp (temp) values (null);
+insert into main.temp (temp) values (null);
+insert into main.temp (temp) values (null);
+insert into main.temp (temp) values (null);
+insert into main.temp (temp) values (null);
+insert into main.temp (temp) values (null);
+insert into main.temp (temp) values (null);
+insert into main.temp (temp) values (null);
+insert into main.temp (temp) values (null);
+insert into main.temp (temp) values (null);
+insert into main.temp (temp) values (null);
+insert into main.temp (temp) values (null);
+insert into main.temp (temp) values (null);
+insert into main.temp (temp) values (null);
+insert into main.temp (temp) values (null);
